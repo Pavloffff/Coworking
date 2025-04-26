666c8a6071c7531da64061ac95d5e1e8f9c843f2
Pavloffff <pavlov.id.2003@gmail.com>
2025-04-26 20:15:09 +0300

    bugs logging and fix

--- source/database_configurator/python/database_configurator/repositories/user_server.py
+++ source/database_configurator/python/database_configurator/repositories/user_server.py
@@ -13,23 +13,25 @@ class UserServerRepository(BaseRepository):
         roles_stmt = roles_stmt.where(Role.name == 'common')
         roles_response = await session.execute(statement=roles_stmt)
         role = roles_response.scalar_one_or_none()
-        user_name, user_tag = data['user_data'].split('#')
-        user_stmt = select(User).where(User.name == user_name)
-        user_response = await session.scalars(statement=user_stmt)
-        users = user_response.all()
-        target_user = None
-        for user in users:
-            if user.tag % 10000 == int(user_tag):
-                target_user = user
-                break
-        if target_user is not None:
-            await UserRoleRepository.insert(
-                session=session,
-                data={
-                    'user_id': user.user_id,
-                    'role_id': role.role_id
-                }
-            )
+        user_data = data.get('user_data')
+        if user_data is not None and '#' in user_data:
+            user_name, user_tag = data['user_data'].split('#')
+            user_stmt = select(User).where(User.name == user_name)
+            user_response = await session.scalars(statement=user_stmt)
+            users = user_response.all()
+            target_user = None
+            for user in users:
+                if user.tag % 10000 == int(user_tag):
+                    target_user = user
+                    break
+            if target_user is not None:
+                await UserRoleRepository.insert(
+                    session=session,
+                    data={
+                        'user_id': user.user_id,
+                        'role_id': role.role_id
+                    }
+                )
 
     @staticmethod
     async def update(session, data):
