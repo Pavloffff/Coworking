c803c821fe707d28c34ae0e3a062a772c39cfbc1
Pavloffff <pavlov.id.2003@gmail.com>
2025-04-26 19:10:02 +0300

    remove localhost

--- source/web/typescript/web/src/config/config.ts
+++ source/web/typescript/web/src/config/config.ts
@@ -14,17 +14,17 @@ export const config: AppConfig = {
 	database_reader_api: {
 		baseUrl:
 			import.meta.env.VITE_DATABASE_READER_API_BASE_URL ||
-			'https://localhost/database-reader/api/v1',
+			'https://coworking.maitopcoders.ru/database-reader/api/v1',
 		timeout: Number(import.meta.env.VITE_API_TIMEOUT) || 5000,
 	},
 	servers_configurator_api: {
 		baseUrl:
 			import.meta.env.VITE_SERVERS_CONFIGURATOR_API_BASE_URL ||
-			'https://localhost/servers-configurator/api/v1',
+			'https://coworking.maitopcoders.ru/servers-configurator/api/v1',
 		timeout: Number(import.meta.env.VITE_API_TIMEOUT) || 5000,
 	},
 	notifications_pisher_ws_endpoint: `${
 		import.meta.env.VITE_NOTIFICATIONS_PUSHER_API_BASE_URL ||
-		'wss://localhost/notifications-pusher/api/v1'
+		'wss://coworking.maitopcoders.ru/notifications-pusher/api/v1'
 	}/notifications`,
 }
